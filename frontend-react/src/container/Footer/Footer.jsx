6a9a17d549b2708400afef3c0ac454cb83ee50ff
Craig Burke <cragardev@gmail.com>
2022-07-05 19:00:05 -0500

    footer

--- frontend-react/src/container/Footer/Footer.jsx
+++ frontend-react/src/container/Footer/Footer.jsx
@@ -1,10 +1,104 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { images } from '../../constants'
+import { AppWrap, MotionWrap } from '../../wrapper'
+import { client } from '../../client'
 import './Footer.scss'
 
 const Footer = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+  const { name, email, message } = formData
+
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  const handleChangeInput = e => {
+    console.log('handleChangeInput', e.target.value)
+    const { name, value } = e.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const handleSubmit = () => {
+    console.log('handleSubmit')
+    setLoading(true)
+
+    const contact = {
+      _type: 'contact',
+      name: name,
+      email: email,
+      message: message
+    }
+
+    client.create(contact).then(() => {
+      setLoading(false)
+      setIsFormSubmitted(true)
+    })
+  }
   return (
-    <div>Footer</div>
+    <>
+      <h2 className='head-text'>Grab a coffee and let's chat...</h2>
+      <div className='app__footer-cards'>
+        <div className='app__footer-card'>
+          <img src={images.mobile} alt='mobile' />
+          <a href='tel:+1 (214) 957-0713' className='p-text'>
+            cel: +1 (214) 957-0713
+          </a>
+        </div>
+        <div className='app__footer-card'>
+          <img src={images.email} alt='email' />
+          <a href='mailto:cragardev@gmail.comn' className='p-text'>
+            email: cragardev@gmail.com
+          </a>
+        </div>
+      </div>
+
+      {!isFormSubmitted ? (
+        <div className='app__footer-form app__flex'>
+          <div className='app__flex'>
+            <input
+              className='p-text'
+              type='text'
+              placeholder='Your Name'
+              name='name'
+              value={name}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div className='app__flex'>
+            <input
+              className='p-text'
+              type='text'
+              placeholder='Your Email'
+              name='email'
+              value={email}
+              onChange={handleChangeInput}
+            />
+          </div>
+          <div>
+            <textarea
+              className='p-text'
+              name='message'
+              value={message}
+              onChange={handleChangeInput}
+              placeholder='Your Message'
+            ></textarea>
+          </div>
+          <button className='p-text' type='button' onClick={handleSubmit}>
+            {loading ? 'SENDING' : 'Send Message'}
+          </button>
+        </div>
+      ) : (
+        <div className='head-text'>Thank You for getting in touch!</div>
+      )}
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+)
--- frontend-react/src/wrapper/MotionWrap.js
+++ frontend-react/src/wrapper/MotionWrap.js
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const MotionWrap = (Component, classNames) =>
   function HOC() {
     return (
-      <motion.div whileInView={{ y: [0, 0, 1], opacity: [0, 0, 1] }} duration={{ duration: 0.5 }} classNames={`${classNames} app__flex`}>
+      <motion.div whileInView={{ y: [0, 0, 1], opacity: [0, 0, 1] }} duration={{ duration: 0.5 }} className={`${classNames} app__flex`}>
         <Component />
       </motion.div>
     );
